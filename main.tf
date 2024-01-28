8308291129a716968fb3ecacd7091b5a1b2c65cf
rightfittech <rightfit.technology@gmail.com>
2024-01-28 22:21:22 +0000

    adding infra

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.43.0"
+    }
+  }
+  cloud {
+    organization = "rightfittech"
+
+    workspaces {
+      name = "Hands-On_With_Terraform_On_Azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+  skip_provider_registration = true
+
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "813-174553b7-hands-on-with-terraform-on-azure"
+  location = "East US"
+}
+
+module "securestorage" {
+  source               = "app.terraform.io/rightfittech/securestorage/azurerm"
+  version              = "1.0.0"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "memaemimm123"
+}
